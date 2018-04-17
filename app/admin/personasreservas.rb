890d12a48496c3b672fc1c3455f84afea968bff0
jvignola <godsavethebufon@hotmail.com>
2018-04-17 20:08:53 -0300

    agrego entidades a active admin

---
+++ app/admin/personasreservas.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Personasreserva do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+
+permit_params :reserva_id, :email, :telefono, :celular, :nombre1, :nombre2, :apellido1, :apellido2
+
+end
---
+++ app/admin/reservas.rb
@@ -0,0 +1,17 @@
+ActiveAdmin.register Reserva do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+permit_params :usuario_id, :empresa_id,  :evento_id, :eventoinstancium_id, :precio, :cantidadpersonas, :codigo
+
+end
